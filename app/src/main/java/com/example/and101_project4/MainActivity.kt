cdb540f6e8533acf32e8f15ef9f88289237c1899
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-10-12 02:13:49 -0400

    done with core features

--- app/src/main/java/com/example/and101_project4/MainActivity.kt
+++ app/src/main/java/com/example/and101_project4/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.and101_project4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextWatcher
 import android.widget.SeekBar
 import com.example.and101_project4.databinding.ActivityMainBinding
@@ -24,10 +25,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         binding.seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
                 var percentageString: String  = progress.toString()
-                binding.tvPercent.setText(percentageString + "%")
+                binding.tvPercent.text = percentageString + "%"
+                compute()
 
 
 
@@ -43,9 +46,42 @@ class MainActivity : AppCompatActivity() {
 
         })
 
+        binding.etInput.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(input: Editable?) {
+                compute()
 
 
 
+            }
+        })
+
+
+    }
+    private fun compute() {
+
+        if(binding.etInput.text.isEmpty()){
+           binding.tvTotalAmount.text = ""
+            binding.tvTipAmount.text = ""
+            return
+
+        }
+
+        var baseInfo = binding.etInput.text.toString().toDouble()
+        var seekBarInfo = binding.seekBarTip.progress
+
+        var tipTotal = (baseInfo * seekBarInfo)/100
+        var tipTotalAmount = baseInfo + tipTotal
+
+        binding.tvTipAmount.text = tipTotal.toString()
+        binding.tvTotalAmount.text = tipTotalAmount.toString()
 
 
     }
